20a74b5ea96df2c0b36253fe0ef2ee0a7af5a57b
mgpatil4418 <mgpatil4418@gmail.com>
2025-03-27 21:51:01 +0530

    Add files via upload

---
+++ calculatornum.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        // Prompt the user to enter the operator
+        System.out.print("Enter the operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Perform the operation using a switch statement
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator. Please enter +, -, *, or /.");
+                return;
+        }
+
+        // Print the result
+        System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+
+        // Close the Scanner object
+        scanner.close();
+    }
+}
+
